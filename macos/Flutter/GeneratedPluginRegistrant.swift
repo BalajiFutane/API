2184145aa2f3c0828c69e30d1c2829d0c3d0eca7
BalajiFutane <balajifutane1@gmail.com>
2024-01-29 21:00:50 +0530

    Fisnesh Code

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_pay_upi
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterPayUpiPlugin.register(with: registry.registrar(forPlugin: "FlutterPayUpiPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_pay_upi/flutter_pay_upi_plugin_c_api.h>
+#include <url_launcher_windows/url_launcher_windows.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterPayUpiPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterPayUpiPluginCApi"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
+}
